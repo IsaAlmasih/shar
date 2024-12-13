ffe661c61fa207f59dd29a7ccf62e5ff6cd64d80
Savva <imperiya66.ru@gmail.com>
2024-12-13 16:17:10 +0300

    prod2сам

--- src/app/abaut/page.js
+++
@@ -1,8 +0,0 @@
-
-import styles from "./abaut.module.css";
-
-export default function Abaut() {
-  return (
-    <div className={styles.wrapper}><p>Клеемашина.</p></div>
-  );
-}
--- src/app/page.js
+++ src/app/page.js
@@ -4,24 +4,9 @@ import styles from "./styles.module.css";
 export default function Home() {
   return (
     <div className={styles.wrapper}>
-      <div className={styles.intro}>
-        <div>Главная</div>
-        <div>Контакты</div>
-        <div>О нас</div>
-      </div>
-      <div className={styles.wrappe}></div>
-      <div>
-        Сайт в разработке. Если вы в поиске вашего запроса, свяжитесь по
-        контактам ниже.
-      </div>
-      <div>
-        {" "}
-        <p>
-          119071, Москва, ул Орджоникидзе, 10 Тел.: +7 (977) 424-00-79
-          (WhatsApp) E-mail: info@sharov-tech.com Добро пожаловать на наш сайт!
-        </p>
-      </div>
-      <div>Блок 5</div>
+      <div>SHAROV technologies.</div>
+      <div></div>
+      <div></div>
     </div>
   );
 }
