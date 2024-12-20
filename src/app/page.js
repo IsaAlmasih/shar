c03d06f1932d19d81ed848b0ee66d6f97a4f94ff
Savva <imperiya66.ru@gmail.com>
2024-12-20 14:57:08 +0300

    savva

--- src/app/page.js
+++ src/app/page.js
@@ -56,15 +56,15 @@ export default function Home() {
                 "https://www.google.com/maps/search/119071,+%D0%9C%D0%BE%D1%81%D0%BA%D0%B2%D0%B0,%20%D1%83%D0%BB%20%D0%9E%D1%80%D0%B4%D0%B6%D0%BE%D0%BD%D0%B8%D0%BA%D0%B8%D0%B4%D0%B7%D0%B5,%2010,"
               }
             >
-              119071, Москва, ул Орджоникидзе, 10
+              119071, Москва, ул Электролитный проезд, 16А (Магазин ГРАЙ второй этаж)
             </a>
           </div>
           <div>
-            Тел: <a href={"tel:+79774240079"}>+7(977)424-00-79(WhatsApp)</a>
+            Тел: <a href={"tel:+79774240079"}>+7(977)424-00-79 (WhatsApp)</a>
           </div>
           <div>
             email:
-            <a href={"mailto:info@sharov-tech.com"}>info@sharov-tech.com</a>
+            <a href={"mailto:info@sharov-tech.com"}>  23v@mail.ru</a>
           </div>
         </div>
         <div className={styles.about}>
